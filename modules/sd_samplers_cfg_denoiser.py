f50bbb7192a4ecc475af68934ff15441102a0512
Panchovix <panchovixmenn@gmail.com>
2024-07-06 19:44:01 -0400

    Make refiner switchover based on model timesteps instead of sampling steps

--- modules/sd_samplers_cfg_denoiser.py
+++ modules/sd_samplers_cfg_denoiser.py
@@ -165,7 +165,7 @@ class CFGDenoiser(torch.nn.Module):
             x = x * (((real_sigma ** 2.0 + real_sigma_data ** 2.0) ** 0.5)[:, None, None, None])
             sigma = real_sigma
 
-        if sd_samplers_common.apply_refiner(self, x):
+        if sd_samplers_common.apply_refiner(self, x, sigma):
             cond = self.sampler.sampler_extra_args['cond']
             uncond = self.sampler.sampler_extra_args['uncond']
 
