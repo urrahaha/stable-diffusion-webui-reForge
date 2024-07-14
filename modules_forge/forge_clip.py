77dc80f365c45cd250671f81d34c52ec51e02d9b
Panchovix <panchovixmenn@gmail.com>
2024-07-14 04:20:40 -0400

    Experimental: Move text projection into the CLIP code 2 Changes seeds. Experimental since I had to do a lot of changes on forge_loader.py to make this work. For now, ignore "clip unexpected: ['clip_g.transformer.text_projection.weight']"

--- modules_forge/forge_clip.py
+++ modules_forge/forge_clip.py
@@ -95,9 +95,10 @@ class CLIP_SD_XL_G(FrozenCLIPEmbedderWithCustomWords):
 
         pooled_output = outputs.pooler_output
 
-        if hasattr(self.wrapped, 'text_projection'):
+        if hasattr(self.wrapped, 'text_projection') and self.wrapped.text_projection is not None:
             text_projection = self.wrapped.text_projection
             pooled_output = pooled_output.float().to(text_projection.device) @ text_projection.float()
-        
+
         z.pooled = pooled_output
         return z
+    
