0d7e8b7d0a6e685f00b33d1c0da557c29ea31102
Panchovix <francisco.jaqueat@gmail.com>
2024-07-20 19:35:01 -0400

    New Samplers: ODE Bosh3, ODE Fehlberg2, ODE Adaptive Heun andODE Dopri5 Live previews and console log doesn't seem to work with these particular samplers, you will get just the final image. Sometimes it can make you think it gave the same before as before, but it's because the webui sometimes get stuck with these samplers, they get auto saved anyways.

--- ldm_patched/k_diffusion/sampling.py
+++ ldm_patched/k_diffusion/sampling.py
@@ -13,6 +13,7 @@ from tqdm.auto import trange, tqdm
 from ldm_patched.modules import utils
 from ldm_patched.k_diffusion import deis
 import ldm_patched.modules.model_patcher
+import torchdiffeq
 
 def append_zero(x):
     return torch.cat([x, x.new_zeros([1])])
@@ -64,6 +65,130 @@ def get_ancestral_step(sigma_from, sigma_to, eta=1.):
 
 def default_noise_sampler(x):
     return lambda sigma, sigma_next: torch.randn_like(x)
+
+ADAPTIVE_SOLVERS = { "dopri8", "dopri5", "bosh3", "fehlberg2", "adaptive_heun" }
+FIXED_SOLVERS = { "euler", "midpoint", "rk4", "heun3", "explicit_adams", "implicit_adams" }
+class ODEFunction:
+    def __init__(self, model, t_min, t_max, n_steps, is_adaptive, extra_args=None, callback=None):
+        self.model = model
+        self.extra_args = {} if extra_args is None else extra_args
+        self.callback = callback
+        self.t_min = t_min.item()
+        self.t_max = t_max.item()
+        self.n_steps = n_steps
+        self.is_adaptive = is_adaptive
+        self.step = 0
+
+        if is_adaptive:
+            self.pbar = tqdm(
+                total=100,
+                desc="solve",
+                unit="%",
+                leave=False,
+                position=1
+            )
+        else:
+            self.pbar = tqdm(
+                total=n_steps,
+                desc="solve",
+                leave=False,
+                position=1
+            )
+
+    def __call__(self, t, y):
+        if t <= 1e-5:
+            return torch.zeros_like(y)
+
+        denoised = self.model(y.unsqueeze(0), t.unsqueeze(0), **self.extra_args)
+        return (y - denoised.squeeze(0)) / t
+
+    def _callback(self, t0, y0, step):
+        if self.callback is not None:
+            y0 = y0.unsqueeze(0)
+
+            self.callback({
+                "x": y0,
+                "i": step,
+                "sigma": t0,
+                "sigma_hat": t0,
+                "denoised": y0, # for a bad latent preview
+            })
+
+    def callback_step(self, t0, y0, dt):
+        if self.is_adaptive:
+            return
+
+        self._callback(t0, y0, self.step)
+
+        self.pbar.update(1)
+        self.step += 1
+
+    def callback_accept_step(self, t0, y0, dt):
+        if not self.is_adaptive:
+            return
+
+        progress = (self.t_max - t0.item()) / (self.t_max - self.t_min)
+
+        self._callback(t0, y0, round((self.n_steps - 1) * progress))
+
+        new_step = round(100 * progress)
+        self.pbar.update(new_step - self.step)
+        self.step = new_step
+
+    def reset(self):
+        self.step = 0
+        self.pbar.reset()
+
+class ODESampler:
+    def __init__(self, solver, rtol, atol, max_steps):
+        self.solver = solver
+        self.rtol = rtol
+        self.atol = atol
+        self.max_steps = max_steps
+
+    @torch.no_grad()
+    def __call__(self, model, x: torch.Tensor, sigmas: torch.Tensor, extra_args=None, callback=None, disable=None):
+        t_max = sigmas.max()
+        t_min = sigmas.min()
+        n_steps = len(sigmas)
+
+        if self.solver in FIXED_SOLVERS:
+            t = sigmas
+            is_adaptive = False
+        else:
+            t = torch.stack([t_max, t_min])
+            is_adaptive = True
+
+        ode = ODEFunction(model, t_min, t_max, n_steps, is_adaptive=is_adaptive, callback=callback, extra_args=extra_args)
+
+        samples = torch.empty_like(x)
+        for i in trange(x.shape[0], desc=self.solver, disable=disable):
+            ode.reset()
+
+            samples[i] = torchdiffeq.odeint(
+                ode,
+                x[i],
+                t,
+                rtol=self.rtol,
+                atol=self.atol,
+                method=self.solver,
+                options={
+                    "min_step": 1e-5,
+                    "max_num_steps": self.max_steps,
+                    "dtype": torch.float32 if torch.backends.mps.is_available() else torch.float64
+                }
+            )[-1]
+
+        if callback is not None:
+            callback({
+                "x": samples,
+                "i": n_steps - 1,
+                "sigma": t_min,
+                "sigma_hat": t_min,
+                "denoised": samples, # only accurate if t_min = 0, for now
+            })
+
+        return samples
 
 
 class BatchedBrownianTree:
@@ -1189,6 +1314,12 @@ def sample_dpmpp_2m_cfg_pp(model, x, sigmas, extra_args=None, callback=None, dis
             x = denoised + to_d(x, sigmas[i], denoised_d) * sigmas[i + 1]
         old_denoised = temp[0]
     return x
+
+@torch.no_grad()
+def sample_ode(model, x, sigmas, extra_args=None, callback=None, disable=None, solver="dopri5", rtol=1e-3, atol=1e-4, max_steps=250):
+    """Implements ODE-based sampling."""
+    sampler = ODESampler(solver, rtol, atol, max_steps)
+    return sampler(model, x, sigmas, extra_args=extra_args, callback=callback, disable=disable)
 
 # @torch.no_grad()
 # def sample_dpmpp_2m_sde_cfg_pp(model, x, sigmas, extra_args=None, callback=None, disable=None, eta=1., s_noise=1., noise_sampler=None, solver_type='midpoint'):
