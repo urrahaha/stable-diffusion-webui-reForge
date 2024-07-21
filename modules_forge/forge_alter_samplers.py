332a1c389c015812b42e5aa48d517aa9fa775693
Panchovix <francisco.jaqueat@gmail.com>
2024-07-21 02:33:35 -0400

    Fix ode_custom sampler

--- modules_forge/forge_alter_samplers.py
+++ modules_forge/forge_alter_samplers.py
@@ -36,6 +36,7 @@ class AlterSampler(sd_samplers_kdiffusion.KDiffusionSampler):
             'ode_fehlberg2': self.sample_ode_fehlberg2,
             'ode_adaptive_heun': self.sample_ode_adaptive_heun,
             'ode_dopri5': self.sample_ode_dopri5,
+            'ode_custom':self.sample_ode_custom,
         }
         
         sampler_function = sampler_functions.get(sampler_name)
