4f1a654f3282b31c7728f373ce6278661cce0472
Panchovix <francisco.jaqueat@gmail.com>
2024-07-27 16:49:01 -0400

    Upstream forge clip from Forge Illya mentioned: "Technically Correct Implementation of CLIP skip for all models. Automatic1111 uses a weird logic to ignore SDXL clip skip but added an option to only set CLIP-L Skip with an offset 1, which means if one set skip as 3, then the SDXL CLIP G does not skip but SDXL CLIP L uses 3-1=2 as skip (not 3?!) Forge now uses technically correct and consistent clip skip for all models. Forge now outweigh technical correctness over reproducing Auto results. But one can get same results by just do not set clip skip for SDXL.
    less aggressive clip skip to make CivitAI pony image meta works better"

--- modules_forge/forge_clip.py
+++ modules_forge/forge_clip.py
@@ -14,12 +14,16 @@ def move_clip_to_gpu():
 
 
 class CLIP_SD_15_L(FrozenCLIPEmbedderWithCustomWords):
+    def __init__(self, wrapped, hijack):
+        super().__init__(wrapped, hijack)
+        self.minimal_clip_skip = 1
+
     def encode_with_transformers(self, tokens):
         move_clip_to_gpu()
         self.wrapped.transformer.text_model.embeddings.to(tokens.device)
         outputs = self.wrapped.transformer(input_ids=tokens, output_hidden_states=-opts.CLIP_stop_at_last_layers)
 
-        if opts.CLIP_stop_at_last_layers > 1:
+        if opts.CLIP_stop_at_last_layers > self.minimal_clip_skip:
             z = outputs.hidden_states[-opts.CLIP_stop_at_last_layers]
             z = self.wrapped.transformer.text_model.final_layer_norm(z)
         else:
@@ -39,13 +43,17 @@ class CLIP_SD_21_H(FrozenCLIPEmbedderWithCustomWords):
         self.id_start = 49406
         self.id_end = 49407
         self.id_pad = 0
+        self.minimal_clip_skip = 2
 
     def encode_with_transformers(self, tokens):
         move_clip_to_gpu()
         self.wrapped.transformer.text_model.embeddings.to(tokens.device)
         outputs = self.wrapped.transformer(tokens, output_hidden_states=self.wrapped.layer == "hidden")
 
-        if self.wrapped.layer == "last":
+        if opts.CLIP_stop_at_last_layers > self.minimal_clip_skip:
+            z = outputs.hidden_states[-opts.CLIP_stop_at_last_layers]
+            z = self.wrapped.transformer.text_model.final_layer_norm(z)
+        elif self.wrapped.layer == "last":
             z = outputs.last_hidden_state
         else:
             z = outputs.hidden_states[self.wrapped.layer_idx]
@@ -57,12 +65,13 @@ class CLIP_SD_21_H(FrozenCLIPEmbedderWithCustomWords):
 class CLIP_SD_XL_L(FrozenCLIPEmbedderWithCustomWords):
     def __init__(self, wrapped, hijack):
         super().__init__(wrapped, hijack)
+        self.minimal_clip_skip = 2
 
     def encode_with_transformers(self, tokens):
         self.wrapped.transformer.text_model.embeddings.to(tokens.device)
         outputs = self.wrapped.transformer(tokens, output_hidden_states=self.wrapped.layer == "hidden")
 
-        if opts.sdxl_clip_l_skip is True:
+        if opts.CLIP_stop_at_last_layers > self.minimal_clip_skip:
             z = outputs.hidden_states[-opts.CLIP_stop_at_last_layers]
         elif self.wrapped.layer == "last":
             z = outputs.last_hidden_state
@@ -83,12 +92,15 @@ class CLIP_SD_XL_G(FrozenCLIPEmbedderWithCustomWords):
         self.id_start = 49406
         self.id_end = 49407
         self.id_pad = 0
+        self.minimal_clip_skip = 2
 
     def encode_with_transformers(self, tokens):
         self.wrapped.transformer.text_model.embeddings.to(tokens.device)
         outputs = self.wrapped.transformer(tokens, output_hidden_states=self.wrapped.layer == "hidden")
 
-        if self.wrapped.layer == "last":
+        if opts.CLIP_stop_at_last_layers > self.minimal_clip_skip:
+            z = outputs.hidden_states[-opts.CLIP_stop_at_last_layers]
+        elif self.wrapped.layer == "last":
             z = outputs.last_hidden_state
         else:
             z = outputs.hidden_states[self.wrapped.layer_idx]
@@ -98,3 +110,4 @@ class CLIP_SD_XL_G(FrozenCLIPEmbedderWithCustomWords):
         pooled_output = pooled_output.float().to(text_projection.device) @ text_projection.float()
         z.pooled = pooled_output
         return z
+    
