03cbc2fb6a54979d237002d930d20b31234deb81
Panchovix <francisco.jaqueat@gmail.com>
2024-10-17 15:47:58 -0300

    Cleanup https://github.com/comfyanonymous/ComfyUI/commit/935ae153e154813ace36db4c4656a5e96f403eba#diff-791d4d41d3718d15d49180f3aacc8370b8cab07383f0d35b2713651cc0adfe46L55

--- ldm_patched/modules/model_management.py
+++ ldm_patched/modules/model_management.py
@@ -193,6 +193,7 @@ VAE_DTYPES = get_optimal_vae_dtype()
 try:
     if is_nvidia():
             xpu_available = False
+    torch_version = ""
     try:
         torch_version = torch.version.__version__
         xpu_available = (int(torch_version[0]) < 2 or (int(torch_version[0]) == 2 and int(torch_version[2]) <= 4)) and torch.xpu.is_available()
