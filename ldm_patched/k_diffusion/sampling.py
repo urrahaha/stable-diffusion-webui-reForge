d75abf72e9a9acc05b03c8e0280fed33de867a2e
Panchovix <francisco.jaqueat@gmail.com>
2024-10-17 16:04:50 -0300

    Doesn't really make sense for cfg_pp sampler to call regular one https://github.com/comfyanonymous/ComfyUI/commit/9f4daca9d9994c600ef713856e0182d0177b9a1e#diff-98c58fed7d8a86bbb3bd3d0a76d3851541753a00676fe4cabca3e3be8616da32R1105

--- ldm_patched/k_diffusion/sampling.py
+++ ldm_patched/k_diffusion/sampling.py
@@ -504,8 +504,6 @@ def sample_euler_ancestral(model, x, sigmas, extra_args=None, callback=None, dis
 
 @torch.no_grad()
 def sample_dpmpp_2s_ancestral_cfg_pp(model, x, sigmas, extra_args=None, callback=None, disable=None, eta=1., s_noise=1., noise_sampler=None):
-    if isinstance(model.inner_model.inner_model.model_sampling, ldm_patched.modules.model_sampling.CONST):
-        return sample_dpmpp_2s_ancestral_RF(model, x, sigmas, extra_args, callback, disable, eta, s_noise, noise_sampler)
     
     """Ancestral sampling with DPM-Solver++(2S) second-order steps."""
     extra_args = {} if extra_args is None else extra_args
