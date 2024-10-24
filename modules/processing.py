ef9736c6c3f06d47ed53d262d100ef1168a3cfed
catboxanon <122327233+catboxanon@users.noreply.github.com>
2024-10-24 11:22:54 -0400

    Fix application of ztSNR

--- modules/processing.py
+++ modules/processing.py
@@ -958,13 +958,13 @@ if opts.sd_processing == "reForge OG":
                 if p.n_iter > 1:
                     shared.state.job = f"Batch {n+1} out of {p.n_iter}"
 
-                # TODO: This currently seems broken. It should be fixed or fixed.
-                sd_models.apply_alpha_schedule_override(p.sd_model, p)
-
                 sigmas_backup = None
+                alphas_cumprod_backup = None
                 if (opts.sd_noise_schedule == "Zero Terminal SNR" or (hasattr(p.sd_model, 'ztsnr') and p.sd_model.ztsnr)) and p is not None:
-                    p.extra_generation_params['Noise Schedule'] = opts.sd_noise_schedule
                     sigmas_backup = p.sd_model.forge_objects.unet.model.model_sampling.sigmas
+                    alphas_cumprod_backup = p.sd_model.alphas_cumprod
+                    p.sd_model.alphas_cumprod_original = p.sd_model.alphas_cumprod
+                    sd_models.apply_alpha_schedule_override(p.sd_model, p)
                     p.sd_model.forge_objects.unet.model.model_sampling.set_sigmas(rescale_zero_terminal_snr_sigmas(p.sd_model.forge_objects.unet.model.model_sampling.sigmas))
 
                 samples_ddim = p.sample(conditioning=p.c, unconditional_conditioning=p.uc, seeds=p.seeds, subseeds=p.subseeds, subseed_strength=p.subseed_strength, prompts=p.prompts)
@@ -974,6 +974,8 @@ if opts.sd_processing == "reForge OG":
 
                 if sigmas_backup is not None:
                     p.sd_model.forge_objects.unet.model.model_sampling.set_sigmas(sigmas_backup)
+                if alphas_cumprod_backup is not None:
+                    p.sd_model.alphas_cumprod = alphas_cumprod_backup
 
                 if p.scripts is not None:
                     ps = scripts.PostSampleArgs(samples_ddim)
@@ -2799,13 +2801,13 @@ elif opts.sd_processing == "reForge A1111":
                 if p.n_iter > 1:
                     shared.state.job = f"Batch {n+1} out of {p.n_iter}"
 
-                # TODO: This currently seems broken. It should be fixed or fixed.
-                sd_models.apply_alpha_schedule_override(p.sd_model, p)
-
                 sigmas_backup = None
+                alphas_cumprod_backup = None
                 if (opts.sd_noise_schedule == "Zero Terminal SNR" or (hasattr(p.sd_model, 'ztsnr') and p.sd_model.ztsnr)) and p is not None:
-                    p.extra_generation_params['Noise Schedule'] = opts.sd_noise_schedule
                     sigmas_backup = p.sd_model.forge_objects.unet.model.model_sampling.sigmas
+                    alphas_cumprod_backup = p.sd_model.alphas_cumprod
+                    p.sd_model.alphas_cumprod_original = p.sd_model.alphas_cumprod
+                    sd_models.apply_alpha_schedule_override(p.sd_model, p)
                     p.sd_model.forge_objects.unet.model.model_sampling.set_sigmas(rescale_zero_terminal_snr_sigmas(p.sd_model.forge_objects.unet.model.model_sampling.sigmas))
 
                 with devices.without_autocast() if devices.unet_needs_upcast else devices.autocast():
@@ -2816,6 +2818,8 @@ elif opts.sd_processing == "reForge A1111":
 
                 if sigmas_backup is not None:
                     p.sd_model.forge_objects.unet.model.model_sampling.set_sigmas(sigmas_backup)
+                if alphas_cumprod_backup is not None:
+                    p.sd_model.alphas_cumprod = alphas_cumprod_backup
 
                 if p.scripts is not None:
                     ps = scripts.PostSampleArgs(samples_ddim)
