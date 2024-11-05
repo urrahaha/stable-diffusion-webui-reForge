04084224a89efbca7a734ac2ae96f6c6a31f3af4
Shinon <contakt@shinon71.moe>
2024-11-05 16:19:51 +0800

    Pull in updates from https://github.com/MythicalChu/ComfyUI-APG_ImYourCFGNow

--- extensions-builtin/reForge-APGIsYourCFG/scripts/advanced_model_sampling_script.py
+++ extensions-builtin/reForge-APGIsYourCFG/scripts/advanced_model_sampling_script.py
@@ -7,8 +7,8 @@ from APGIsYourCFG.nodes_APGImYourCFGNow import APG_ImYourCFGNow
 class APGIsNowYourCFGScript(scripts.Script):
     def __init__(self):
         self.enabled = False
-        self.apg_scale = 9.0
-        self.apg_moment = -0.05
+        self.apg_moment = 0.5
+        self.apg_adaptive_moment = 0.180
         self.apg_norm_thr = 15.0
         self.apg_eta = 1.0
 
@@ -24,19 +24,19 @@ class APGIsNowYourCFGScript(scripts.Script):
         with gr.Accordion(open=False, label=self.title()):
             gr.HTML("<p><i>Adjust the settings for APG's now your CFG.</i></p>")
             enabled = gr.Checkbox(label="Enable APG's now your CFG", value=self.enabled)
-            apg_scale = gr.Slider(
-                label="APG Scale",
-                minimum=0.0,
-                maximum=100.0,
-                step=0.1,
-                value=self.apg_scale,
-            )
             apg_momentum = gr.Slider(
                 label="APG Momentum",
                 minimum=-1.5,
                 maximum=0.5,
                 step=0.01,
                 value=self.apg_moment,
+            )
+            apg_adaptive_momentum = gr.Slider(
+                label="APG Momentum",
+                minimum=-1.5,
+                maximum=0.5,
+                step=0.01,
+                value=self.apg_adaptive_moment,
             )
             apg_norm_thr = gr.Slider(
                 label="APG Norm Threshold",
@@ -51,7 +51,7 @@ class APGIsNowYourCFGScript(scripts.Script):
 
         enabled.change(lambda x: self.update_enabled(x), inputs=[enabled])
 
-        return (enabled, apg_scale, apg_momentum, apg_norm_thr, apg_eta)
+        return (enabled, apg_momentum, apg_adaptive_momentum, apg_norm_thr, apg_eta)
 
     def update_enabled(self, value):
         self.enabled = value
@@ -60,8 +60,8 @@ class APGIsNowYourCFGScript(scripts.Script):
         if len(args) >= 5:
             (
                 self.enabled,
-                self.apg_scale,
                 self.apg_moment,
+                self.apg_adaptive_moment,
                 self.apg_norm_thr,
                 self.apg_eta,
             ) = args[:5]
@@ -80,14 +80,18 @@ class APGIsNowYourCFGScript(scripts.Script):
             return
 
         unet = APG_ImYourCFGNow().patch(
-            unet, self.apg_scale, self.apg_moment, self.apg_norm_thr, self.apg_eta
+            unet,
+            momentum=self.apg_moment,
+            adaptive_momentum=self.apg_moment,
+            norm_threshold=self.apg_norm_thr,
+            eta=self.apg_eta
         )[0]
 
         p.sd_model.forge_objects.unet = unet
         args = {
             "apgisyourcfg_enabled": True,
-            "apgisyourcfg_scale": self.apg_scale,
             "apgisyourcfg_momentum": self.apg_moment,
+            "apgisyourcfg_adaptive_momentum": self.apg_adaptive_moment,
             "apgisyourcfg_norm_thr": self.apg_norm_thr,
             "apgisyourcfg_eta": self.apg_eta,
         }
