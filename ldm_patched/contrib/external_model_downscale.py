e486abe5e29c8d63bb2bb2e2dd28b3fe7503b390
Panchovix <francisco.jaqueat@gmail.com>
2024-11-11 11:58:44 -0300

    Try: Update external_model_downscale/kohya hi-res fix

--- ldm_patched/contrib/external_model_downscale.py
+++ ldm_patched/contrib/external_model_downscale.py
@@ -21,11 +21,12 @@ class PatchModelAddDownscale:
     RETURN_TYPES = ("MODEL",)
     FUNCTION = "patch"
 
-    CATEGORY = "_for_testing"
+    CATEGORY = "model_patches/unet"
 
     def patch(self, model, block_number, downscale_factor, start_percent, end_percent, downscale_after_skip, downscale_method, upscale_method):
-        sigma_start = model.model.model_sampling.percent_to_sigma(start_percent)
-        sigma_end = model.model.model_sampling.percent_to_sigma(end_percent)
+        model_sampling = model.get_model_object("model_sampling")
+        sigma_start = model_sampling.percent_to_sigma(start_percent)
+        sigma_end = model_sampling.percent_to_sigma(end_percent)
 
         def input_block_patch(h, transformer_options):
             if transformer_options["block"][1] == block_number:
