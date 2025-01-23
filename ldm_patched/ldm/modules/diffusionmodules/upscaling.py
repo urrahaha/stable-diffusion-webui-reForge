2dd4ab201c9b5b7b50c05bee9e60b0133e6e3e34
Panchovix <francisco.jaqueat@gmail.com>
2025-01-23 18:09:24 -0300

    Credits

--- ldm_patched/ldm/modules/diffusionmodules/upscaling.py
+++ ldm_patched/ldm/modules/diffusionmodules/upscaling.py
@@ -3,7 +3,7 @@
 # 3rd edit by https://github.com/Stability-AI/generative-models
 # 4th edit by https://github.com/comfyanonymous/ComfyUI
 
-# This file is only for reference, and not used in the backend or runtime.
+
 
 
 import torch
